cd817a46c6bb90047854a21679099b93c85f91aa
Maciej Stanek <makaron28@gmail.com>
2022-12-03 22:36:01 +0100

    02: Implemented the second part.

--- 02/solution.cxx
+++ 02/solution.cxx
@@ -3,32 +3,65 @@
 #include <string>
 #include <vector>
 
-enum Shape { Rock, Paper, Scissors };
+enum class Shape { Rock, Paper, Scissors };
+
+Shape winning_shape(Shape shape)
+{
+  return shape == Shape::Rock ? Shape::Paper
+      : shape == Shape::Paper ? Shape::Scissors
+                              : Shape::Rock;
+}
+
+Shape loosing_shape(Shape shape)
+{
+  return shape == Shape::Rock ? Shape::Scissors
+      : shape == Shape::Paper ? Shape::Rock
+                              : Shape::Paper;
+}
 
 int shape_score(Shape shape) { return static_cast<int>(shape) + 1; }
 
 struct Duel {
   Shape player;
   Shape oponent;
-};
-
-int duel_score(Duel duel)
-{
-  auto const& oponent = duel.oponent;
-  auto const& player = duel.player;
-  auto constexpr DEFEAT_SCORE = 0;
-  auto constexpr DRAW_SCORE = 3;
-  auto constexpr WIN_SCORE = 6;
-  if (oponent == player) {
-    return DRAW_SCORE;
-  } else if ((player == Shape::Scissors && oponent == Shape::Paper)
-      || (player == Shape::Paper && oponent == Shape::Rock)
-      || (player == Shape::Rock && oponent == Shape::Scissors)) {
-    return WIN_SCORE;
-  } else {
-    return DEFEAT_SCORE;
+  struct Result {
+    enum Value { Win, Draw, Defeat };
+    Result() = default;
+    constexpr Result(Value value_)
+        : value { value_ }
+    {
+    }
+    constexpr operator Value() const { return value; }
+    constexpr operator int() const { return value == Win ? 6 : value == Draw ? 3 : 0; }
+    explicit operator bool() const = delete;
+    Value value;
+  };
+  Result expected_result() const
+  {
+    return player == Shape::Rock ? Result::Defeat
+        : player == Shape::Paper ? Result::Draw
+                                  : Result::Win;
   }
-}
+  Shape expected_player_move() const
+  {
+    auto const result = expected_result();
+    return result == Result::Win ? winning_shape(oponent)
+        : result == Result::Draw ? oponent
+                                 : loosing_shape(oponent);
+  }
+  Result score() const
+  {
+    if (oponent == player) {
+      return Result::Draw;
+    } else if ((player == Shape::Scissors && oponent == Shape::Paper)
+        || (player == Shape::Paper && oponent == Shape::Rock)
+        || (player == Shape::Rock && oponent == Shape::Scissors)) {
+      return Result::Win;
+    } else {
+      return Result::Defeat;
+    }
+  }
+};
 
 using Strategy = std::vector<Duel>;
 
@@ -61,11 +94,21 @@ Strategy parse_input(std::istream& input)
 int solve(Strategy const& strategy)
 {
   return std::transform_reduce(strategy.begin(), strategy.end(), 0, std::plus<>(),
-      [](auto const& duel) { return duel_score(duel) + shape_score(duel.player); });
+      [](auto const& duel) { return static_cast<int>(duel.score()) + shape_score(duel.player); });
+}
+
+int solve_part_2(Strategy const& strategy)
+{
+  return std::transform_reduce(
+      strategy.begin(), strategy.end(), 0, std::plus<>(), [](auto const& duel) {
+        return static_cast<int>(duel.expected_result()) + shape_score(duel.expected_player_move());
+      });
 }
 
 int main()
 {
-  std::cout << solve(parse_input(std::cin)) << "\n";
+  auto input = parse_input(std::cin);
+  std::cout << "part 1: " << solve(input) << "\n";
+  std::cout << "part 2: " << solve_part_2(input) << "\n";
   return 0;
 }
