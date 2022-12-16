f9a0e7b5fb174cc4fb1c8a0bc43380889c583426
Maciej Stanek <makaron28@gmail.com>
2022-12-17 00:57:15 +0100

    08: Solved the scenic view calculations.

--- 08/solution.cxx
+++ 08/solution.cxx
@@ -1,27 +1,36 @@
 #include <algorithm>
 #include <array>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <string>
 #include <vector>
 
-using Row = std::string;
+using Row = std::vector<int>;
 using Forest = std::vector<Row>;
 
 Forest parse(std::istream& input)
 {
   Forest rows;
   while (!input.eof()) {
-    Row row;
-    std::getline(input, row);
-    if (row.empty()) {
+    std::string line;
+    std::getline(input, line);
+    if (line.empty()) {
       break;
     }
-    std::transform(row.cbegin(), row.cend(), row.begin(),
+    Row row;
+    std::transform(line.cbegin(), line.cend(), std::back_inserter(row),
         [](auto tree) { return tree - '0'; });
     rows.push_back(row);
   }
   return rows;
+}
+
+std::ostream& operator<<(std::ostream& output, Row const& row)
+{
+  std::transform(row.cbegin(), row.cend(), std::ostream_iterator<char>(output),
+      [](auto value) -> char { return value + '0'; });
+  return output;
 }
 
 std::ostream& operator<<(std::ostream& output, Forest const& forest)
@@ -93,35 +102,74 @@ Rotations mark_trees_from_left(Rotations rotations)
   return rotations;
 }
 
-Row combine_masks(Row mask1, Row const& mask2)
+template <typename Op>
+Row combine_masks(Row mask1, Row const& mask2, Op const& op)
 {
-  std::transform(mask1.cbegin(), mask1.cend(), mask2.cbegin(), mask1.begin(),
-      std::logical_or {});
+  std::transform(
+      mask1.cbegin(), mask1.cend(), mask2.cbegin(), mask1.begin(), op);
   return mask1;
 }
 
-Forest combine_masks(Forest mask1, Forest const& mask2)
+template <typename Op>
+Forest combine_masks(Forest mask1, Forest const& mask2, Op const& op)
 {
   std::transform(mask1.cbegin(), mask1.cend(), mask2.cbegin(), mask1.begin(),
-      [](auto left, auto right) { return combine_masks(left, right); });
+      [&op](auto left, auto right) { return combine_masks(left, right, op); });
   return mask1;
 }
 
-Forest combine_rotations(Rotations rotations)
+template <typename Op>
+Forest combine_rotations(Rotations rotations, Op const& op)
 {
   auto masks = undo_rotations(rotations);
   return std::reduce(std::next(masks.cbegin()), masks.cend(), masks[0],
-      [](auto const& left, auto const& right) {
-        return combine_masks(left, right);
+      [&op](auto const& left, auto const& right) {
+        return combine_masks(left, right, op);
       });
 }
 
 int solve(Forest forest)
 {
-  auto mask = combine_rotations(mark_trees_from_left(make_rotations(forest)));
-  return std::transform_reduce(mask.cbegin(), mask.cend(), 0, std::plus{},
-      [](auto const& row) {
+  auto mask = combine_rotations(
+      mark_trees_from_left(make_rotations(forest)), std::logical_or {});
+  return std::transform_reduce(
+      mask.cbegin(), mask.cend(), 0, std::plus {}, [](auto const& row) {
         return std::accumulate(row.cbegin(), row.cend(), 0);
+      });
+}
+
+Row calculate_view(Row row)
+{
+  for (auto i = row.begin(); i != row.end(); ++i) {
+    auto j = std::find_if(
+        std::next(i), row.end(), [&i](auto tree) { return tree >= *i; });
+    *i = std::distance(i, j) + (j == row.end() ? -1 : 0);
+  }
+  return row;
+}
+
+Forest calculate_view(Forest forest)
+{
+  std::transform(forest.cbegin(), forest.cend(), forest.begin(),
+      [](auto const& row) { return calculate_view(row); });
+  return forest;
+}
+
+Rotations calculate_view(Rotations rotations)
+{
+  std::transform(rotations.cbegin(), rotations.cend(), rotations.begin(),
+      [](auto const& forest) { return calculate_view(forest); });
+  return rotations;
+}
+
+int solve_view(Forest forest)
+{
+  constexpr const auto max = [](int a, int b) { return std::max(a, b); };
+  auto mask = combine_rotations(
+      calculate_view(make_rotations(forest)), std::multiplies {});
+  return std::transform_reduce(
+      mask.cbegin(), mask.cend(), 0, max, [&max](auto const& row) {
+        return std::reduce(row.cbegin(), row.cend(), 0, max);
       });
 }
 
@@ -129,6 +177,15 @@ int main()
 {
   auto forest = parse(std::cin);
   std::cout << forest << "\n";
-  std::cout << solve(forest) << "\n";
+  std::cout << "cover " << solve(forest) << "\n";
+  std::cout << "right " << calculate_view(forest) << "\n";
+  std::cout << "left " << mirror(calculate_view(mirror(forest))) << "\n";
+  std::cout << "down " << flip(calculate_view(flip(forest))) << "\n";
+  std::cout << "up " << flip(mirror(calculate_view(mirror(flip(forest)))))
+            << "\n";
+  std::cout << "combined "
+            << combine_rotations(
+                   calculate_view(make_rotations(forest)), std::multiplies {});
+  std::cout << "scenic score " << solve_view(forest) << "\n";
   return 0;
 }
