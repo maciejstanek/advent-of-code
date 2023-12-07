4cb119025b86ac8b2083624269d0a18541ff00aa
maciejstanek <eszpag@gmail.com>
2023-12-07 22:51:08 +0100

    Added a WIP 'ranges' solution for part two

--- 2023/01/solution.cxx
+++ 2023/01/solution.cxx
@@ -26,9 +26,40 @@ auto main() -> int {
         return base * first + last;
     };
 
-    auto const sum = std::ranges::fold_left(
-        getlines(std::cin) | std::views::transform(parse_coord), 0, std::plus<int>());
-
+    auto const lines = getlines(std::cin);
+    auto const sum =
+        std::ranges::fold_left(lines | std::views::transform(parse_coord), 0, std::plus<int>());
     std::cout << sum << '\n';
+
+    auto const tokens = std::vector<std::string>{
+        "0",   "1",   "2",     "3",    "4",    "5",   "6",     "7",     "8",   "9",
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    auto const reverse = [](std::string const& value) -> std::string { return {value.rbegin(), value.rend()}; };
+    auto const reversed_tokens_view = tokens | std::views::transform(reverse);
+    auto const reversed_tokens = std::vector<std::string>{
+        std::ranges::begin(reversed_tokens_view),
+        std::ranges::end(reversed_tokens_view)};
+
+    // TODO: How to change parameter type to accept both lists?
+    //       auto const reversed_tokens = tokens | std::views::transform(reverse);
+    auto const parse_extended_coord = [](decltype(tokens) const& tokens) {
+        return [&tokens](std::string const& line) {
+            auto const find_position = [&line](std::string const& token) {
+                return line.find(token);
+            };
+            auto found_view = tokens | std::views::transform(find_position);
+            auto const min_elem = std::ranges::min_element(found_view);
+            auto const pos = std::ranges::distance(std::ranges::begin(found_view), min_elem);
+            return pos > 9 ? pos - 9 : pos + 1;
+        };
+    };
+
+    auto firsts = lines | std::views::transform(parse_extended_coord(tokens));
+    auto lasts = lines | std::views::transform(reverse) | std::views::transform(parse_extended_coord(reversed_tokens));
+
+    auto const firsts_sum = std::ranges::fold_left(firsts, 0, std::plus<int>());
+    auto const lasts_sum = std::ranges::fold_left(lasts, 0, std::plus<int>());
+    std::cout << 10 * firsts_sum + lasts_sum << '\n';
+
     return 0;
 }
