bb833931fc6d24190de8e897f82cc7cbafc4bf1e
maciejstanek <eszpag@gmail.com>
2023-12-08 13:03:49 +0100

    Added the solution for day two

--- 2023/02/solution.cxx
+++ 2023/02/solution.cxx
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -18,36 +19,34 @@ auto getlines(std::istream& input) -> Lines {
 struct Pull {
     int r, g, b;
 };
+using Pulls = std::vector<Pull>;
 
 struct Game {
     int id;
-    std::vector<Pull> pulls;
+    Pulls pulls;
 };
-
 using Games = std::vector<Game>;
 
 auto parse_pull(Line const& str) -> Pull {
     Pull pull{};
     auto iter = str.begin();
     while (iter != str.end()) {
-        auto num_iter = std::find_if(iter, str.end(), static_cast<int(*)(int)>(std::isdigit));
+        auto num_iter = std::find_if(iter, str.end(), static_cast<int (*)(int)>(std::isdigit));
         auto num_iter_end = std::find(num_iter, str.end(), ' ');
-        auto type_iter = std::find_if(num_iter_end, str.end(), static_cast<int(*)(int)>(std::isalpha));
+        auto type_iter =
+            std::find_if(num_iter_end, str.end(), static_cast<int (*)(int)>(std::isalpha));
         auto num = std::atoi(std::string{num_iter, num_iter_end}.c_str());
         switch (*type_iter) {
             case 'r': {
                 pull.r = num;
-                std::cout << "R = " << num << "\n";
                 break;
             }
             case 'g': {
                 pull.g = num;
-                std::cout << "G = " << num << "\n";
                 break;
             }
             case 'b': {
                 pull.b = num;
-                std::cout << "B = " << num << "\n";
                 break;
             }
         }
@@ -63,7 +62,7 @@ auto parse_games(Lines const& lines) -> Games {
         auto id_str = std::string(std::next(std::find(line.begin(), iter, ' ')), iter);
         games.push_back({std::atoi(id_str.c_str()), {}});
         while (iter != line.end()) {
-            iter = std::find_if(iter, line.end(), static_cast<int(*)(int)>(std::isdigit));
+            iter = std::find_if(iter, line.end(), static_cast<int (*)(int)>(std::isdigit));
             auto end_iter = std::find(iter, line.end(), ';');
             games.back().pulls.push_back(parse_pull({iter, end_iter}));
             iter = end_iter;
@@ -73,6 +72,18 @@ auto parse_games(Lines const& lines) -> Games {
 }
 
 auto main() -> int {
-    parse_games(getlines(std::cin));
+    auto const games = parse_games(getlines(std::cin));
+    auto const is_possible = [](Pull const& pull) {
+        auto const max_pull = Pull{12, 13, 14};
+        return pull.r <= max_pull.r && pull.g <= max_pull.g && pull.b <= max_pull.b;
+    };
+    auto const are_possible = [&is_possible](Pulls const& pulls) {
+        return std::all_of(pulls.begin(), pulls.end(), is_possible);
+    };
+    auto const to_factor = [&are_possible](Game const& game) {
+        return are_possible(game.pulls) ? game.id : 0;
+    };
+    auto result = std::transform_reduce(games.cbegin(), games.cend(), 0, std::plus<>(), to_factor);
+    std::cout << result << '\n';
     return 0;
 }
