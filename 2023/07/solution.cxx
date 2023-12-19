d1b03db7b7955455fb2381bc3350dbf8794bbd4c
maciejstanek <eszpag@gmail.com>
2023-12-19 14:05:05 +0100

    Added solution for day 7 part 2

--- 2023/07/solution.cxx
+++ 2023/07/solution.cxx
@@ -27,55 +27,87 @@ struct Hand {
 };
 using Hands = std::vector<Hand>;
 
-auto rate_hand(Hand const& hand) -> int {
+/*
+Adding jokers to the hand
+
+AAAAA/
+ * No jokers possible
+AAAA/B
+ * AAAAJ -> AAAAA -> #A+1
+AAABB/
+ * No jokers possible
+AAA/BC
+ * AAAJB -> AAAAB -> #A+1
+ * AAAJJ -> AAAAA -> #A+2
+AABB/C
+ * AABBJ -> AAABB -> #A+1
+AA/BCD
+ * AAJBC -> AAABC -> #A+1
+ * AAJJB -> AAAAB -> #A+2
+ * AAJJJ -> AAAAA -> #A+3
+A/BCDE
+ * AJBCD -> AABCD -> #A+1
+ * AJJBC -> AAABC -> #A+2
+ * AJJJB -> AAAAB -> #A+3
+ * AJJJJ -> AAAAA -> #A+4
+
+Summary: add #jokers to the top count
+
+Plus special case all JJJJJ
+*/
+
+auto rate_hand(Hand const& hand, bool joker) -> int {
     std::map<int, int> mapping;
-    std::cout << "rate_hand:";
-    for (auto x : hand.cards) {
-        std::cout << " " << x;
-    }
-    std::cout << "\n";
+    auto jokers = 0;
     for (auto const& card : hand.cards) {
-        ++mapping[card];
+        if (joker && card == 11) {
+            ++jokers;
+        } else {
+            ++mapping[card];
+        }
     }
     std::vector<int> powers;
-    std::cout << " * powers:";
     for (auto const& [card, count] : mapping) {
         powers.push_back(count);
-        std::cout << " (" << card << " #" << count << ")";
     }
-    std::cout << "\n";
     std::sort(powers.rbegin(), powers.rend());
-    std::cout << " * sorted powers:";
-    for (auto x : powers) {
-        std::cout << " " << x;
+    if (powers.empty()) {
+        powers.push_back(0);
     }
-    std::cout << "\n";
-    auto p = powers[0] == 5                   ? 7 :
-             powers[0] == 4                   ? 6 :
-             powers[0] == 3 && powers[1] == 2 ? 5 :
-             powers[0] == 3 && powers[1] == 1 ? 4 :
-             powers[0] == 2 && powers[1] == 2 ? 3 :
-             powers[0] == 2 && powers[1] == 1 ? 2 :
-                                                1;
-    std::cout << " * points " << p << "\n";
+    powers[0] += jokers;
+    auto p = powers.size() == 0 || powers[0] == 5 ? 7 :
+             powers[0] == 4                       ? 6 :
+             powers[0] == 3 && powers[1] == 2     ? 5 :
+             powers[0] == 3 && powers[1] == 1     ? 4 :
+             powers[0] == 2 && powers[1] == 2     ? 3 :
+             powers[0] == 2 && powers[1] == 1     ? 2 :
+                                                    1;
     return p;
 }
-auto rate_hand_tiebreaker(Hand const& hand) -> int {
+auto rate_hand_tiebreaker(Hand const& hand, int jvalue) -> int {
     const auto base = 14;
     auto checksum = 0;
     for (auto card : hand.cards) {
-        checksum = checksum * base + card;
+        checksum = checksum * base + (card == 11 ? jvalue : card);
     }
     return checksum;
 }
 
-auto operator<(Hand const& a, Hand const& b) -> bool {
-    auto const a_rating = rate_hand(a);
-    auto const b_rating = rate_hand(b);
+auto compare(Hand const& a, Hand const& b, int jvalue, bool joker) -> bool {
+    auto const a_rating = rate_hand(a, joker);
+    auto const b_rating = rate_hand(b, joker);
     return a_rating < b_rating ? true :
            a_rating > b_rating ? false :
-                                 rate_hand_tiebreaker(a) <
-                                     rate_hand_tiebreaker(b);
+                                 rate_hand_tiebreaker(a, jvalue) <
+                                     rate_hand_tiebreaker(b, jvalue);
+}
+
+auto joker_aware_compare(Hand const& a, Hand const& b) -> bool {
+    return compare(a, b, 1, true);
+}
+
+auto operator<(Hand const& a, Hand const& b) -> bool {
+    return compare(a, b, 11, false);
 }
 
 auto debug_print(Hand const& hand) -> void {
@@ -106,18 +138,22 @@ auto parse(std::istream& input) -> Hands {
     return hands;
 }
 
-auto main() -> int {
-    auto hands = parse(std::cin);
-    std::sort(hands.begin(), hands.end());
-    debug_print(hands);
-    auto sum = std::ranges::fold_left(
+auto rate(Hands const& hands) -> int {
+    return std::ranges::fold_left(
         std::views::transform(
             hands, [](Hand const& hand) { return hand.bet; }) |
             std::views::enumerate | std::views::transform([](auto const& kv) {
                 return (std::get<0>(kv) + 1) * std::get<1>(kv);
             }),
         0, std::plus{});
-    std::cout << sum << std::endl;
+}
 
+auto main() -> int {
+    auto hands = parse(std::cin);
+    std::sort(hands.begin(), hands.end());
+    std::cout << rate(hands) << std::endl;
+    std::sort(hands.begin(), hands.end(), joker_aware_compare);
+    debug_print(hands);
+    std::cout << rate(hands) << std::endl;
     return 0;
 }
