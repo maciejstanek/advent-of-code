e7dbf394962e75d4d34490d02a9b42104246ec2e
maciejstanek <eszpag@gmail.com>
2024-12-13 19:22:47 +0100

    Factored out the reactor safety function

--- 2024/day02/src/main.rs
+++ 2024/day02/src/main.rs
@@ -15,24 +15,30 @@ fn parse() -> Vec<Vec<i32>> {
     inputs
 }
 
+fn is_safe(sequence: Vec<i32>) -> bool {
+    let result = sequence
+        .windows(2)
+        .map(|ab| {
+            let [a, b] = ab else {
+                todo!();
+            };
+            *b - *a
+        })
+        .map(|x| if x > 3 || x < -3 { 0 } else { x })
+        .collect::<Vec<_>>();
+
+    let has_zero = result.iter().any(|x| *x == 0);
+    let has_positive = result.iter().any(|x| *x > 0);
+    let has_negative = result.iter().any(|x| *x < 0);
+
+    !has_zero && has_positive != has_negative
+}
+
 fn main() {
     let inputs = parse();
     let mut sum_safe: i32 = 0;
     for input in inputs {
-        let result = input
-            .windows(2)
-            .map(|ab| {
-                let [a, b] = ab else {
-                    todo!();
-                };
-                *b - *a
-            })
-            .map(|x| if x > 3 || x < -3 { 0 } else { x })
-            .collect::<Vec<_>>();
-        let has_zero = result.iter().any(|x| *x == 0);
-        let has_positive = result.iter().any(|x| *x > 0);
-        let has_negative = result.iter().any(|x| *x < 0);
-        if !has_zero && has_positive != has_negative {
+        if is_safe(input) {
             sum_safe += 1;
         }
     }
