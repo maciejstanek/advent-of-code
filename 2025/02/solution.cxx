a8df8c38b251e1e46c91a17c0ed60cfca4e375c5
maciejstanek <eszpag@gmail.com>
2025-12-07 20:41:03 +0100

    AoC 2025 Day 2 Part 2'

--- 2025/02/solution.cxx
+++ 2025/02/solution.cxx
@@ -28,17 +28,55 @@ auto parse(std::istream& input) -> Pairs {
     return result;
 }
 
-auto invalid(Int val) -> bool
+auto invalid1(Int val) -> bool
 {
     const auto str = std::to_string(val);
     return (str.size() % 2 == 1) ? false : (str.substr(0, str.size() / 2 ) == str.substr(str.size() / 2));
+}
+
+auto LIMIT = 20;
+
+auto is_repetition_of(std::string const& text, std::string const& atom) -> bool
+{
+    if (text.length() % atom.length()) {
+        return false;
+    }
+    for (auto i = atom.length(); i < text.length(); i += atom.length()) {
+        if (atom != text.substr(i, atom.length())) {
+            return false;
+        }
+    }
+    return true;
+}
+
+auto invalid2(Int val) -> bool
+{
+    const auto str = std::to_string(val);
+    for (auto current_length = 1ull; current_length < str.length(); ++current_length) {
+        auto atom = str.substr(0, current_length);
+        if (is_repetition_of(str, atom)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+auto solve2(Pair const& p) -> Int
+{
+    Int sum = 0;
+    for (auto i = p.first; i <= p.second; ++i) {
+        if (invalid2(i)) {
+            sum += i;
+        }
+    }
+    return sum;
 }
 
 auto solve1(Pair const& p) -> Int
 {
     Int sum = 0;
     for (auto i = p.first; i <= p.second; ++i) {
-        if (invalid(i)) {
+        if (invalid1(i)) {
             sum += i;
         }
     }
@@ -52,8 +90,16 @@ auto solve1(Pairs const& pairs) -> Int
     });
 }
 
+auto solve2(Pairs const& pairs) -> Int
+{
+    return std::transform_reduce(pairs.begin(), pairs.end(), Int{}, std::plus{}, [](Pair const& p) -> Int {
+        return solve2(p);
+    });
+}
+
 auto main() -> int {
     const auto x = parse(std::cin);
     std::cout << solve1(x) << std::endl;
+    std::cout << solve2(x) << std::endl;
     return 0;
 }
